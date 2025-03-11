cfad651799b92658d340d594ceb7ba35cf33f0d1
mgonzalezrivero <m.gonzalezrivero@aims.gov.au>
2025-03-11 16:20:57 +1000

    f

--- install_dependencies.R
+++ install_dependencies.R
@@ -22,9 +22,9 @@ install_if_missing <- function(packages) {
     if (!require(pkg, character.only = TRUE)) {
       install.packages(pkg, dependencies = TRUE)
       library(pkg, character.only = TRUE)
-      if (pkg %in% c("h3js","h3" )){
+      if (pkg %in% c("h3js","h3", "rgdal" )){
         devtools::install_github("saurfang/h3js")
-        remotes::install_github("crazycapivara/h3-r")  
+        remotes::install_github("crazycapivara/h3-r") 
       }
     }
   }
